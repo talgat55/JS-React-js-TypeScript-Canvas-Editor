302ea9d3e69d8891c17e2c5c230cd65989834460
talgat55 <lightx_design@yahoo.com>
2021-03-17 00:14:00 +0600

    add needed libraries and add input file also add load file and template konva

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {Stage, Layer,Image, Text} from "react-konva";
 import './App.css';
+const App = () => {
+    const [stateImage , setStateImage] = useState<CanvasImageSource | null | undefined>(null);
+    const  handlerAddImage =(e: React.ChangeEvent<HTMLInputElement>)=>{
+        let input = e.target;
+        if (input?.files?.length) {
+            let reader = new FileReader();
+            reader.onload = async (event: any) => {
+                if(event !==undefined){
+                    let imageObj = new window.Image();
+                    imageObj.onload = function () {
+                        console.log(imageObj)
+                        setStateImage(imageObj);
+                    };
+                    imageObj.src = event?.target?.result;
+                }
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    return (
+        <div className="app">
+            <Stage width={500} height={400}>
+                <Layer>
+                    {
+                        stateImage !== null ?
+                            <Image
+                                image={stateImage}
+                            />
+                            :
+                            null
+                    }
+                    <Text text="Try click on rect"/>
+                </Layer>
+            </Stage>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <input
+                type="file"
+                name="images[]"
+                accept="image/jpeg,image/png,image/gif"
+                onChange={handlerAddImage}/>
+        </div>
+    );
 }
 
 export default App;
