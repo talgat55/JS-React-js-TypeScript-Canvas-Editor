57a975b3afd0d197a0439d467c29849cbbc17368
talgat55 <lightx_design@yahoo.com>
2021-03-18 00:41:21 +0600

    in process build change filter image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {Stage, Layer, Image, Text} from "react-konva";
+import React, {useEffect, useState,useRef} from 'react';
+import {Stage, Layer, Image} from "react-konva";
 import './App.css';
 
 interface ISizesImageElement {
@@ -8,11 +8,30 @@ interface ISizesImageElement {
 }
 
 const App = () => {
+    const stageRef = useRef<any | null>(null);
     const [stateImage, setStateImage] = useState<CanvasImageSource | null | undefined>(null);
-    const [sizesImage, setizesImage] = useState<ISizesImageElement>({
+    const [sizesImage, seSizesImage] = useState<ISizesImageElement>({
         width: 0,
         height: 0
     });
+
+    useEffect(()=>{
+        if(stateImage !==null){
+
+        }
+    },[stateImage])
+
+    const handlerChooseEffect = (effect: string) =>{
+        if(effect === 'ocean'){
+            stageRef?.current.toImage({
+                callback(img: HTMLImageElement) {
+                    console.log(img)
+                    let imageDataFiltered = window?.pixelsJS?.filterImgData(img, "ocean");
+                }
+            });
+        }
+    }
+
     const handlerAddImage = (e: React.ChangeEvent<HTMLInputElement>) => {
         let input = e.target;
         if (input?.files?.length) {
@@ -22,7 +41,7 @@ const App = () => {
                     let imageObj = new window.Image();
                     imageObj.onload = function () {
                         setStateImage(imageObj);
-                        setizesImage(
+                        seSizesImage(
                             {
                                 width: imageObj.width,
                                 height: imageObj.height,
@@ -37,23 +56,37 @@ const App = () => {
     }
     return (
         <div className="app">
-            <Stage width={500} height={400}>
-                <Layer>
-                    {
-                        stateImage !== null ?
-                            <Image
-                                image={stateImage}
-                                cropWidth={sizesImage.width}
-                                cropHeight={sizesImage.height}
-                                width={500}
-                                height={400}
-                            />
-                            :
-                            null
-                    }
-                    <Text text="Try click on rect"/>
-                </Layer>
-            </Stage>
+            <div className="main_wrapper">
+                <div className="image_block">
+                    <Stage
+                        ref={stageRef}
+                        width={500}
+                        height={400}
+                    >
+                        <Layer>
+                            {
+                                stateImage !== null ?
+                                    <Image
+                                        image={stateImage}
+                                        cropWidth={sizesImage.width}
+                                        cropHeight={sizesImage.height}
+                                        width={500}
+                                        height={400}
+                                    />
+                                    :
+                                    null
+                            }
+                        </Layer>
+                    </Stage>
+                </div>
+                <div>
+                    <div
+                        onClick={()=>{handlerChooseEffect('ocean')}}
+                    >
+                        ocean
+                    </div>
+                </div>
+            </div>
 
             <input
                 type="file"
